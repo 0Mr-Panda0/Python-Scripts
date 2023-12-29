82847bf32570d652354a87c5b49709e7c6000cf1
Karan Behera <42995800+0Mr-Panda0@users.noreply.github.com>
2023-12-29 13:31:08 +0530

    Generates strong and secure password

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,13 @@
-from random import choice as random
+from random import choice, randint
 from string import ascii_letters, digits, punctuation
+from datetime import datetime
+
+def storing_password(password):
+    with open(".\\resources\passwords.txt",'a+') as note:
+        note.write(f"Password:{password} - length of password:{len(password)} - timestamp:{datetime.now()}\n")
 
 if __name__ == "__main__":
     character = digits+ascii_letters+punctuation
-    length_of_password = int(input("Enter length of password:"))
-    password = "".join(random(character)for every_character in range(length_of_password))
-    print(password)
+    length_of_password = randint(8,16)
+    password = "".join(choice(character)for every_character in range(length_of_password))
+    storing_password(password)
